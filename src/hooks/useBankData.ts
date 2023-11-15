5f918382512e74e4dfecfc925a801381b329a67b
gunnardraft <gunnardraft@live.com.mx>
2023-11-15 01:43:10 -0600

    use getServerSideProps

--- src/hooks/useBankData.ts
+++ src/hooks/useBankData.ts
@@ -22,34 +22,22 @@ const useStore = create<IBankingList>((set) => ({
 
 const useBankData = () => {
     const { banks, setBanks } = useStore();
-
-    useEffect(() => {
-        const fetchData = async () => { 
+ 
+    async function getServerSideProps() {
+        const fetchData = async () => {
             try {
-                const response = await axios.get(process.env.URL_BANKS || "dev.obtenmas.com/catom/api/challenge/banks", {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Referrer-Policy': 'no-referrer',
-                        'X-Content-Type-Options': 'nosniff',
-                        'X-Frame-Options': 'deny',
-                        'X-XSS-Protection': '1; mode=block',
-                        'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-                        'Content-Security-Policy': 'default-src \'self\'',
-                        'Feature-Policy': 'geolocation \'self\'; microphone \'none\'',
-                        'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
-                        'Access-Control-Allow-Origin': '*',
-                        'Access-Control-Allow-Methods': 'GET',
-                    },
+                const response = await axios.get(process.env.NEXT_PUBLIC_URL_BANKS, {
+
                     withCredentials: true,
                 });
-                if (response.status === 200) { setBanks(response.data); } 
+                if (response.status === 200) { setBanks(response.data); }
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
         fetchData();
-    }, [setBanks]);
-
+    }
+    getServerSideProps()
     return banks;
 }
 
