bee6d1e3d6f65203c394d63e7592f48e89c3b989
gunnardraft <gunnardraft@live.com.mx>
2023-11-14 21:27:40 -0600

    fix build type in yarn, use npm

--- src/components/BankList.tsx
+++ src/components/BankList.tsx
@@ -1,23 +1,24 @@
-import useBankData from '../hooks/useBankData'
 import { Ul, Li, H1, BankListContent, TitleContent } from '../styles/Styles'
-export default function BankList() {
+import useBankData from '../hooks/useBankData'
+function BankList() {
     const banks = useBankData();
-    return (
-        <BankListContent>
-            <TitleContent>
-                <H1>Lista de bancos</H1>
-            </TitleContent>
-            <Ul>
-                {banks.map((bank) => (
-                    <Li key={bank.bankName}>
-                        <p><b>Banco: </b>{bank.bankName}</p>
-                        <p><b>Descripción: </b>{bank.description}</p>
-                        <p><b>Edad: </b>{bank.age}</p>
-                        <p><b>URL: </b><a href={bank.url}> {bank.url}</a></p>
-                    </Li>
-                ))}
-            </Ul>
-        </BankListContent>
+    return ( 
+             <BankListContent>
+                <TitleContent>
+                    <H1>Lista de bancos</H1>
+                </TitleContent>
+                <Ul>
+                    {banks.map((bank) => (
+                        <Li key={bank.bankName}>
+                            <p><b>Banco: </b>{bank.bankName}</p>
+                            <p><b>Descripción: </b>{bank.description}</p>
+                            <p><b>Edad: </b>{bank.age}</p>
+                            <p><b>URL: </b><a href={bank.url}> {bank.url}</a></p>
+                        </Li>
+                    ))}
+                </Ul>
+             </BankListContent>  
     );
 };
 
+export default BankList;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import BankList from '../components/BankList';
 
 const Home = () => {
